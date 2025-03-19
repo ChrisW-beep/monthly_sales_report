8fb857ac2fd123e7ae5437a8f233bf87b1a99b9f
Chris Wilson <cwilson@posnation.com>
2025-03-19 11:45:24 -0400

    Update monthly_sales_report.py

--- monthly_sales_report.py
+++ monthly_sales_report.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from dbfread import DBF
 
-LOCAL_UNZIPPED_BASE = "/tmp/extracted/6045/"
+LOCAL_UNZIPPED_BASE = "/tmp/extracted/6045/Data"
 REPORT_PATH = "./reports/monthly_sales_report.csv"
 
 
